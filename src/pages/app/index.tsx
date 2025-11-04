acd793e3c903484dcbb73aec0c8b771acf5b342f
softgenai <admin@softgen.ai>
2025-11-04 15:56:47 +0000

    Updates

---
+++ src/pages/app/index.tsx
@@ -0,0 +1,16 @@
+
+    import { GetServerSideProps } from "next";
+    
+    export default function AppIndexRedirect() {
+      return null;
+    }
+    
+    export const getServerSideProps: GetServerSideProps = async () => {
+      return {
+        redirect: {
+          destination: "/app/overview",
+          permanent: false,
+        },
+      };
+    };
+  
